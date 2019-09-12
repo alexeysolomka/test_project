fc5fa684e60e5f0ff97608a73f3924d658143e00
Danylo <brickk1337@gmail.com>
2019-09-12 19:29:54 +0300

    Add Active/Inactive button

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -22,6 +22,13 @@
                     <form method="POST" action="{{ route('users.store') }}">
                         @csrf
                         <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">Is Active</label>
+                            <div class="col-md-6">
+                                Is Active <input type="radio" name="is_active" checked="checked" value="1">
+                                Is in active <input type="radio" name="is_active" checked="checked" value="0">
+                            </div>
+                        </div>
+                        <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
