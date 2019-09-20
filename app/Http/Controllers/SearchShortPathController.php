d684fc1985d910f63a596cca500a19b6fa54aba2
Danylo <brickk1337@gmail.com>
2019-09-20 12:04:05 +0300

    Search shortest way

--- app/Http/Controllers/SearchShortPathController.php
+++ app/Http/Controllers/SearchShortPathController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class SearchShortPathController extends Controller
 {
-    private $paths;
+    private $paths = [];
     private $path;
 
     public function index()
@@ -29,53 +29,15 @@ class SearchShortPathController extends Controller
         $routes = [];
         $stations = Station::all();
         $visited = [];
-        $routes[] = $this->calculateRoutes(null, $stationFrom->id, $stationTo->id, $visited);
-    }
-
-    private function searchRoutes($start, $end, $routes = [])
-    {
-        $startId = $start;
-        $endId = $end;
-        if ($start < $end) {
-            $startId = $start;
-            $endId = $end;
-        } else {
-            $startId = $end;
-            $endId = $start;
+        $this->calculateRoutes(null, $stationFrom->id, $stationTo->id, $visited);
+        if(count($this->paths) > 0)
+        {
+            return min($this->paths);
         }
-
-        $start = Station::find($startId);
-        $end = Station::find($endId);
-
-        $routes[] = $start->name;
-
-        if ($start->id == $end->id) {
-            return $routes;
+        else
+        {
+            return "No need to travel. You are already where you wanted to be :)";
         }
-
-        if (isset($start->next)) {
-            if ($start->intersections->isNotEmpty()) {
-                foreach ($start->intersections as $intersection) {
-                    $stations = $intersection->stations->where('id', '<>', $intersection->station_id);
-                    foreach ($stations as $station) {
-                        $branch_id = $station->branch_id;
-                        $stationsBranch = Station::where('branch_id', $branch_id)->where('id', '>=', $station->id)->get();
-                        foreach ($stationsBranch as $stationBranch) {
-                            if (!in_array($stationBranch->name, $routes)) {
-                                $newRoutes = $this->searchRoutes($stationBranch->id, $end->id, $routes);
-                                foreach ($newRoutes as $newRoute) {
-                                    array_push($routes, $newRoute);
-                                }
-                            }
-                            return $this->searchRoutes($start->next, $end->id, $routes);
-                        }
-                    }
-                }
-            }
-            return $this->searchRoutes($start->next, $end->id, $routes);
-        }
-
-        return $routes;
     }
 
     private function calculateRoutes($previousStation, $currentStation, $destinationStation, $visitedStations = [])
@@ -84,16 +46,15 @@ class SearchShortPathController extends Controller
         $currentStation = Station::find($currentStation);
         $destinationStation = Station::find($destinationStation);
         if ($currentStation->id == $destinationStation->id) {
-            $this->printRoute($destinationStation, $visitedStations);
+            return $this->printRoute($destinationStation, $visitedStations);
         } else {
             if (isset($currentStation->next)) {
                 if (!in_array($currentStation->name, $visitedStations)) {
                     array_push($visitedStations, $currentStation->name);
 
-                    if($currentStation->intersections->isNotEmpty())
-                    {
+                    if ($currentStation->intersections->isNotEmpty()) {
                         foreach ($currentStation->intersections as $intersection) {
-
+                            array_push($visitedStations, $intersection->name);
                             if (isset($previousStation)) {
                                 $stations = $intersection->stations->where('id', '<>', $previousStation->id);
                             } else {
@@ -104,9 +65,7 @@ class SearchShortPathController extends Controller
                                 $this->calculateRoutes($station->id, $station->next, $destinationStation->id, $visitedStations);
                             }
                         }
-                    }
-                    else
-                    {
+                    } else {
                         $this->calculateRoutes($currentStation->id, $currentStation->next, $destinationStation->id, $visitedStations);
                     }
                     array_pop($visitedStations);
@@ -121,10 +80,8 @@ class SearchShortPathController extends Controller
             echo "No need to travel. You are already where you wanted to be :)";
         } else {
             array_push($visitedStations, $destinationStation->name);
-            foreach($visitedStations as $station)
-            {
-                dump($station);
-            }
+            array_push($this->paths, $visitedStations);
+            return $this->paths;
         }
     }
 }
