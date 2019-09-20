05943abbfc4cd3dd14438df7beeb195ba42a3a3e
Danylo <brickk1337@gmail.com>
2019-09-20 16:33:48 +0300

    Working algorithm

--- app/Http/Controllers/SearchShortPathController.php
+++ app/Http/Controllers/SearchShortPathController.php
@@ -30,28 +30,18 @@ class SearchShortPathController extends Controller
         $stations = Station::all();
         $visited = [];
         $this->calculateRoutes(null, $stationFrom->id, $stationTo->id, $visited);
-        if(count($this->paths) > 0)
-        {
+        if (count($this->paths) > 0) {
             return min($this->paths);
-        }
-        else
-        {
+        } else {
             return "No need to travel. You are already where you wanted to be :)";
         }
     }
 
     private function calculateRoutes($previousStation, $currentStation, $destinationStation, $visitedStations = [])
     {
-        $startId = $currentStation;
-        $endId = $destinationStation;
-        if($currentStation > $destinationStation)
-        {
-            $startId = $destinationStation;
-            $endId = $currentStation;
-        }
         $previousStation = Station::find($previousStation);
-        $currentStation = Station::find($startId);
-        $destinationStation = Station::find($endId);
+        $currentStation = Station::find($currentStation);
+        $destinationStation = Station::find($destinationStation);
 
         if ($currentStation->id == $destinationStation->id) {
             return $this->printRoute($destinationStation, $visitedStations);
@@ -59,23 +49,17 @@ class SearchShortPathController extends Controller
             if (isset($currentStation->next)) {
                 if (!in_array($currentStation->name, $visitedStations)) {
                     array_push($visitedStations, $currentStation->name);
-
-                    if ($currentStation->intersections->isNotEmpty()) {
-                        foreach ($currentStation->intersections as $intersection) {
-                            if (isset($previousStation)) {
-                                $stations = $intersection->stations->where('id', '<>', $previousStation->id);
-                            } else {
-                                $stations = $intersection->stations;
-                            }
-
-                            foreach ($stations as $station) {
-                                $this->calculateRoutes($station->id, $station->next, $destinationStation->id, $visitedStations);
-                            }
+                    foreach ($currentStation->intersections as $intersection) {
+                        if (isset($previousStation)) {
+                            $stations = $intersection->stations->where('id', '<>', $previousStation->id);
+                        } else {
+                            $stations = $intersection->stations->where('id', '<>', $currentStation->id);
                         }
-                    } else {
-                        $this->calculateRoutes($currentStation->id, $currentStation->next, $destinationStation->id, $visitedStations);
+                        foreach ($stations as $station) {
+                            $this->calculateRoutes($currentStation->id, $station->id, $destinationStation->id, $visitedStations);
+                        }
                     }
-                    array_pop($visitedStations);
+                    $this->calculateRoutes($currentStation->id, $currentStation->next, $destinationStation->id, $visitedStations);
                 }
             }
         }
