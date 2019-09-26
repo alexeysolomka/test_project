2093644a901ddc367382972c13d7212562492d93
Danylo <brickk1337@gmail.com>
2019-09-26 14:55:16 +0300

    Fix migration

--- database/migrations/2019_09_26_092544_add_phone_number_to_users.php
+++ database/migrations/2019_09_26_092544_add_phone_number_to_users.php
@@ -14,7 +14,7 @@ class AddPhoneNumberToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone_number')->unique();
+            $table->string('phone_number')->unique()->nullable();
         });
     }
 
