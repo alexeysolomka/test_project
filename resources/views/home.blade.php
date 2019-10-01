6450255efb4d2c03a0d38e0b628adc9b9760892a
Danylo <brickk1337@gmail.com>
2019-10-01 12:47:10 +0300

    fix table responsive for users

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,56 +25,58 @@
                         @endif
                         <a href="{{ route('users.profile', ['userId' => auth()->user()->id]) }}">Edit Profile</a>
                     </div>
-                    <table class="table">
-                        <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            @if(auth()->user()->role->name != 'user')
-                                <th scope="col">Delete</th>
-                            @endif
-                            <th scope="col">Edit</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($users as $user)
-                            @if($user->role->name != 'admin')
-                                <tr>
-                                    <th scope="row">{{ $user->id }}</th>
-                                    <td>{{ $user->name }}</td>
-                                    <td>{{ $user->email }}</td>
-                                    @if(auth()->user()->role->name != 'user')
-                                        <td>
-                                            @if($user->id != auth()->user()->id)
-                                                <form method="POST"
-                                                      action="{{ route('users.delete', ['userId' => $user->id]) }}">
-                                                    @csrf
-                                                    <button type="submit"
-                                                            onclick="return confirm('Are you sure want to delete this user?');"
-                                                            class="btn btn-danger">Delete
-                                                    </button>
-                                                </form>
-                                            @endif
-                                        </td>
-                                    @endif
-                                    <td>
-                                        @if(auth()->user()->checkRole('user') && auth()->user()->id == $user->id)
-                                            <a href="{{ route('users.edit', ['userId' => $user->id]) }}"
-                                               class="btn btn-info">Edit</a>
-                                        @elseif(auth()->user()->checkRole('moderator') && $user->role->name != 'moderator')
-                                            <a href="{{ route('users.edit', ['userId' => $user->id]) }}"
-                                               class="btn btn-info">Edit</a>
-                                        @elseif(auth()->user()->checkRole('admin'))
-                                            <a href="{{ route('users.edit', ['userId' => $user->id]) }}"
-                                               class="btn btn-info">Edit</a>
+                    <div class="table-responsive">
+                            <table class="table">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">First</th>
+                                        <th scope="col">Last</th>
+                                        @if(auth()->user()->role->name != 'user')
+                                            <th scope="col">Delete</th>
                                         @endif
-                                    </td>
-                                </tr>
-                            @endif
-                        @endforeach
-                        </tbody>
-                    </table>
+                                        <th scope="col">Edit</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($users as $user)
+                                        @if($user->role->name != 'admin')
+                                            <tr>
+                                                <th scope="row">{{ $user->id }}</th>
+                                                <td>{{ $user->name }}</td>
+                                                <td>{{ $user->email }}</td>
+                                                @if(auth()->user()->role->name != 'user')
+                                                    <td>
+                                                        @if($user->id != auth()->user()->id)
+                                                            <form method="POST"
+                                                                  action="{{ route('users.delete', ['userId' => $user->id]) }}">
+                                                                @csrf
+                                                                <button type="submit"
+                                                                        onclick="return confirm('Are you sure want to delete this user?');"
+                                                                        class="btn btn-danger">Delete
+                                                                </button>
+                                                            </form>
+                                                        @endif
+                                                    </td>
+                                                @endif
+                                                <td>
+                                                    @if(auth()->user()->checkRole('user') && auth()->user()->id == $user->id)
+                                                        <a href="{{ route('users.edit', ['userId' => $user->id]) }}"
+                                                           class="btn btn-info">Edit</a>
+                                                    @elseif(auth()->user()->checkRole('moderator') && $user->role->name != 'moderator')
+                                                        <a href="{{ route('users.edit', ['userId' => $user->id]) }}"
+                                                           class="btn btn-info">Edit</a>
+                                                    @elseif(auth()->user()->checkRole('admin'))
+                                                        <a href="{{ route('users.edit', ['userId' => $user->id]) }}"
+                                                           class="btn btn-info">Edit</a>
+                                                    @endif
+                                                </td>
+                                            </tr>
+                                        @endif
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                    </div>
                     {{ $users->links() }}
                 </div>
             </div>
